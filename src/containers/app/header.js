95109c54398c254661fbbd6fce3e929cc782472a
Guillaume Ader <GAder@esriuk.com>
2018-04-26 18:28:45 +0100

    add menu navigation in drawer

--- src/containers/app/header.js
+++ src/containers/app/header.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 import Home from '../home';
 import About from '../about';
 import { Route, Link } from 'react-router-dom';
@@ -7,22 +10,36 @@ import { Route, Link } from 'react-router-dom';
 class Header extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: false
+    };
   }
 
-  openDrawer(e) {
-    alert('ok');
-  }
+  toggleDrawer = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
 
   render() {
     return (
       <div>
-        <AppBar
-          title="My AppBar"
-          onLeftIconButtonClick={e => this.openDrawer()}
-        />
+        <AppBar title="My AppBar" onLeftIconButtonClick={this.toggleDrawer} />
+        <Drawer open={this.state.open}>
+          <MenuItem onClick={this.toggleDrawer}>
+            <Link to="/">Home</Link>
+          </MenuItem>
+          <MenuItem onClick={this.toggleDrawer}>
+            <Link to="/about-us">About</Link>
+          </MenuItem>
+        </Drawer>
         <header>
-          <Link to="/">Home</Link>
-          <Link to="/about-us">About</Link>
+          <Link to="/" onClick={this.toggleDrawer}>
+            Home
+          </Link>
+          <Link to="/about-us" onClick={this.toggleDrawer}>
+            About
+          </Link>
         </header>
         <main>
           <Route exact path="/" component={Home} />
