e0caf65be94f97cbb4640fa784fdef08461c1976
Guillaume Ader <GAder@esriuk.com>
2018-04-27 17:09:01 +0100

    change ui again

--- src/containers/app/header.js
+++ src/containers/app/header.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
 import { Link } from 'react-router-dom';
+import {
+  Navbar,
+  NavItem,
+  SideNav,
+  SideNavItem,
+  Button
+} from 'react-materialize';
 
 class Header extends Component {
   constructor(props) {
@@ -22,7 +25,41 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <AppBar
+        <Navbar brand="logo" right>
+          <Link to="/">
+            <NavItem onClick={() => console.log('test click')}>
+              Getting started
+            </NavItem>
+          </Link>
+          <Link to="/education">
+            <NavItem href="components.html">Components</NavItem>
+          </Link>
+        </Navbar>
+
+        <SideNav
+          trigger={<Button>SIDE NAV DEMO</Button>}
+          options={{ closeOnClick: true }}>
+          <SideNavItem
+            userView
+            user={{
+              background: 'img/bg.png',
+              image: 'img/me_thumbnail.jpg',
+              name: 'John Doe',
+              email: 'jdandturk@gmail.com'
+            }}
+          />
+          <SideNavItem href="#!icon" icon="cloud">
+            First Link With Icon
+          </SideNavItem>
+          <SideNavItem href="#!second">Second Link</SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem subheader>Subheader</SideNavItem>
+          <SideNavItem waves href="#!third">
+            Third Link With Waves
+          </SideNavItem>
+        </SideNav>
+
+        {/* <AppBar
           title="Guillaume Ader"
           onLeftIconButtonClick={this.toggleDrawer}
         />
@@ -42,7 +79,7 @@ class Header extends Component {
           <Link to="/education">
             <MenuItem onClick={this.toggleDrawer}>Education</MenuItem>
           </Link>
-        </Drawer>
+        </Drawer> */}
       </div>
     );
   }
--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
 import Section from './section';
 import Header from './header';
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 // http://materializecss.com/sidenav.html
 const Main = () => (
-  <MuiThemeProvider>
+  <div>
     <Header />
     <Section />
-    <a className="waves-effect waves-light btn">button</a>
-    <a className="waves-effect waves-light btn">
+    <a className="waves-effect blue-grey lighten-4 btn">button</a>
+    <a className="waves-effect blue-grey lighten-4 btn">
       <i className="material-icons left">cloud</i>button
     </a>
-    <a className="waves-effect waves-light btn">
+    <a className="waves-effect blue-grey lighten-4 btn">
       <i className="material-icons right">cloud</i>button
     </a>
-  </MuiThemeProvider>
+  </div>
 );
 
 export default Main;
